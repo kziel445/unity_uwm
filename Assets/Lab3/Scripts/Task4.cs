ce334e5dce2a72cf01d0fc28f3a82e8e9d114da0
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2021-10-18 21:06:24 +0200

    Task4 (#2)
    * task4 scene
    
    * script to move
    
    * some colors
    
    * task4 done
    
    more colors
    cubes prefabs
    set up colliding with cubes
    
    * comments
    
    * update scene

---
+++ Assets/Lab3/Scripts/Task4.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task4 : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        var collidedWith = collision.gameObject;
+        if(collidedWith.tag=="Obstacle")
+        {
+            //on collision object change color to black
+            collidedWith.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+            //message with what player collided
+            Debug.Log("hit " + collision.gameObject.tag);
+        }
+    }
+    void FixedUpdate()
+    {
+        float mH = Input.GetAxis("Horizontal");
+        float mV = Input.GetAxis("Vertical");
+
+        Vector3 velocity = new Vector3(mH, 0, mV);
+        velocity = velocity.normalized * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + velocity);
+    }
+}
