515cd3bfb2c7d00b538aa06f91f6bab0acc89980
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-10-18 23:04:37 +0200

    unique position spawn

--- Assets/Lab3/Scripts/Task5.cs
+++ Assets/Lab3/Scripts/Task5.cs
@@ -5,61 +5,55 @@ using UnityEngine;
 public class Task5 : MonoBehaviour
 {
     [SerializeField] private GameObject cube;
-    [SerializeField] private float cubeWidth=1;
-    [SerializeField] private float spawnDelay = 1;
+    [SerializeField] private float cubeWidth = 1;
+    [SerializeField] private float spawnDelay = 0.25f;
     [SerializeField] private int amount = 10;
     private float nextSpawnTime;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         spawnInArea(new Vector3(0, 1, 0), new Vector3(10, 1, 10));
     }
-    void Spawn(Vector3 position)
-    {
-        nextSpawnTime = Time.time + spawnDelay;
-        Instantiate(cube, position, transform.rotation);
-    }
     void spawnInArea(Vector3 corner1, Vector3 corner2)
     {
         //cubes are spawned from center, so half of width are changed in max spawn area
-        corner1 += new Vector3(0.5f, 0, 0.5f);
-        corner2 += new Vector3(-0.5f, 0, -0.5f);
+        corner1 += new Vector3(cubeWidth/2, 0, cubeWidth / 2);
+        corner2 += new Vector3(-cubeWidth / 2, 0, -cubeWidth / 2);
         if (amount > 0)
         {
             if (Time.time >= nextSpawnTime)
             {
                 Vector3 randomVector = new Vector3(Random.Range(corner1.x, corner2.x), corner1.y, Random.Range(corner1.z, corner2.z));
-                while (checkIfFree(randomVector, cubeWidth))
-                {
+                do
+                {   
                     if (checkIfFree(randomVector, cubeWidth))
                     {
+                        Debug.Log("Spawn");
                         Spawn(randomVector);
                         amount--;
+                        if (amount == 0) Debug.Log("All cubes spawned");
+                        break;
                     }
+                    Debug.Log("Bad position!");
                     randomVector = new Vector3(Random.Range(corner1.x, corner2.x), corner1.y, Random.Range(corner1.z, corner2.z));
-                };
+                } while (!checkIfFree(randomVector, cubeWidth));
             }
         }
     }
+    void Spawn(Vector3 position)
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        Instantiate(cube, position, transform.rotation);
+    }
     bool checkIfFree(Vector3 center, float cubeWidth)
     {
-        cubeWidth /= 2;
-        Collider[] colliderArray = Physics.OverlapBox(center, new Vector3(cubeWidth, cubeWidth, cubeWidth));
-        Debug.Log(cubeWidth + " " + center);
-        foreach(Collider collider in colliderArray)
+        var checkResult = Physics.OverlapBox(center, new Vector3(cubeWidth / 2, cubeWidth / 2, cubeWidth / 2));
+        if (checkResult.Length == 0)
         {
-            GameObject objects = collider.GetComponent<GameObject>();
-            Debug.Log("something");
-            if (objects != null) return false;
+            return true;
         }
-        return true;
+        return false;
     }
 }
 
