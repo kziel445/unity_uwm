37debb3439cb4304f7ff6718ad0814aac4a60130
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-10-25 16:38:45 +0200

    movement script

--- Assets/Lab4/Scripts/Task4_2.cs
+++ Assets/Lab4/Scripts/Task4_2.cs
@@ -7,13 +7,13 @@ public class Task4_2 : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
+    private float playerSpeed = 4.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
     private void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
     void Update()
