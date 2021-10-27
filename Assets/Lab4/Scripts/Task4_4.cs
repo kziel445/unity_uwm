d99a0535ab856160bca0c70a8cdb73949b5ce626
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-10-27 19:47:48 +0200

    fix camera for task4

--- Assets/Lab4/Scripts/Task4_4.cs
+++ Assets/Lab4/Scripts/Task4_4.cs
@@ -9,11 +9,10 @@ public class Task4_4 : MonoBehaviour
     public Transform player;
 
     public float sensitivity = 200f;
-    float mouseXMove = 0f;
-    float mouseYMove = 0f;
 
     float xMax = 90;
     float xMin = -90;
+    float mouseYMove;
 
     void Start()
     {
@@ -25,18 +24,15 @@ public class Task4_4 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mouseXMove += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        mouseYMove -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-        mouseYMove = Mathf.Clamp(mouseYMove, -90f, 90f);
+        float mouseXMove = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseYMove += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
-        transform.eulerAngles = new Vector3(mouseYMove, 0, 0);
-        // wykonujemy rotację wokół osi Y
-        //player.transform.eulerAngles = new Vector3(0,mouseXMove,0);
+        mouseYMove = Mathf.Clamp(mouseYMove, xMin, xMax);
 
-        // a dla osi X obracamy kamerę dla lokalnych koordynatów
-        // -mouseYMove aby uniknąć ofektu mouse inverse
-        //transform.rotation = Quaternion.Euler(-mouseYMove, 0f, 0f);
-        //transform.Rotate(new Vector3(-mouseYMove, 0f, 0f), Space.Self);
+
+        transform.localRotation = Quaternion.Euler(-mouseYMove, 0f, 0f);
+        player.Rotate(Vector3.up * mouseXMove);
+ 
 
     }
 }
