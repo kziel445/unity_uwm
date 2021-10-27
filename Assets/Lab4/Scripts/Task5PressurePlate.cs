95e679cd4fb108bb025a011c1cdff4ee42b337b2
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-10-27 20:17:57 +0200

    pressure plate

---
+++ Assets/Lab4/Scripts/Task5PressurePlate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task5PressurePlate : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider objectToThrow)
+    {
+        Debug.Log("Hop " + objectToThrow);
+    }
+}
