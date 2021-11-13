33459ffbc1dbcae62d7b0e6d410284a9f23efa4d
Kamil Zieliński <kzielinski445@gmail.com>
2021-11-13 16:28:06 +0100

    concept for task3

---
+++ Assets/Lab5/Scripts/Task3MovingPlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task3MovingPlatform : MonoBehaviour
+{
+    //list of waypoints
+    public Vector3 waypoints;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //foreache waypoint
+        //move to current point 
+    }
+}
