ccc7b2345600c6490851e1a94f9712b95cf7cea3
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-11-15 17:44:12 +0100

    task3 done

--- Assets/Lab5/Scripts/Task3MovingPlatform.cs
+++ Assets/Lab5/Scripts/Task3MovingPlatform.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class Task3MovingPlatform : MonoBehaviour
 {
     //list of waypoints
-    public Vector3 waypoints;
+    public List<Vector3> waypoints;
+    public int waypointCounter=-1;
+    public bool counterAsc = true;
+    public bool isRunning = true;
+    public Vector3 direction;
+    public float speed = 10;
 
-    void Start()
+    private void Awake()
     {
-        
+        waypoints.Insert(0, transform.position);
     }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //foreache waypoint
-        //move to current point 
+        if(isRunning)
+        {
+            if (transform.position == direction)
+            {
+                if (counterAsc)
+                {
+                    waypointCounter++;
+                    if (waypoints.Count-1 <= waypointCounter) counterAsc = false;
+                }
+                else if (!counterAsc)
+                {
+                    waypointCounter--;
+                    if (0 > waypointCounter) counterAsc = true;
+                }
+
+            }
+            direction = waypoints[waypointCounter];
+            Debug.Log(transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, direction, speed * Time.deltaTime);
+        }
     }
 }
