4e01f66eeeaf97b0b8b6a70e788a235b7e16f4f7
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-11-15 16:59:30 +0100

    task2 done

---
+++ Assets/Lab5/Scripts/Task2SlideDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task2SlideDoor : MonoBehaviour
+{
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+    public List<GameObject> doors;
+    public float openWide = 5;
+
+    // Start is called before the first frame update
+
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        foreach(GameObject door in doors)
+        {
+            Task2_2Door doorStatus = door.GetComponent<Task2_2Door>();
+            doorStatus.goingToOpen = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        foreach (GameObject door in doors)
+        {
+            Task2_2Door doorStatus = door.GetComponent<Task2_2Door>();
+            doorStatus.goingToOpen = false;
+        }
+    }
+}
---
+++ Assets/Lab5/Scripts/Task2_2Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task2_2Door : MonoBehaviour
+{
+    private Vector3 startPosition;
+    public Vector3 finalPosition;
+    public bool goingToOpen = false;
+    public bool isRunning = false;
+    public float speed;
+    private Vector3 direction;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        startPosition = transform.position;
+        finalPosition = transform.position + finalPosition;
+    }
+
+    void FixedUpdate()
+    {
+         if (goingToOpen) direction = finalPosition;
+         else direction = startPosition;
+         transform.position = Vector3.MoveTowards(transform.position, direction, speed * Time.deltaTime);
+
+    }
+}
